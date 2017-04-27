7bfe773ecec2d27b7f6bec8df88c29f8f9ca7aec
Atul Sinha <atul_sinha89@yahoo.co.in>
2017-04-27 18:37:24 +0530

    added views and urls as per tutorial

--- mindlog/polls/views.py
+++ mindlog/polls/views.py
@@ -6,7 +6,28 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
-
+from models import Question
+from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+    'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/details.html', {'question': question})
+
+def results(request):
+    return HttpResponse("You are looking at results no - 1")
+
+def vote(request):
+    return HttpResponse("You are looking at vote no - 1")
